277a9ea18e2356a43cad72cb7db1783b24cf61c4
XLazer4 <khannarahul446@gmail.com>
2023-06-09 15:34:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// Check if MetaMask is installed
+if (typeof window.ethereum !== "undefined") {
+  console.log("MetaMask is installed!");
+}
+
+// Function to connect to Polygon network
+async function connectToPolygon() {
+  // Request access to the user's MetaMask accounts
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+
+  // Get the current network ID
+  const chainId = await window.ethereum.request({ method: "eth_chainId" });
+
+  // Check if the current network is Polygon (Matic)
+  if (chainId === "0x89" || chainId === "137") {
+    console.log("Connected to Polygon network!");
+    // You can perform further actions on the Polygon network here
+  } else {
+    // Switch to Polygon network in MetaMask
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x89" }], // Polygon Mainnet
+      });
+      console.log("Switched to Polygon network!");
+      // You can perform further actions on the Polygon network here
+    } catch (error) {
+      console.log("Failed to switch to Polygon network:", error);
+    }
+  }
+}
