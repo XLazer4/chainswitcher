fcdba85989d99eedc0e9250bb0bc12ee0e420238
XLazer4 <khannarahul446@gmail.com>
2023-06-09 17:15:58 +0530

    updated logic

--- app.js
+++ app.js
@@ -5,79 +5,46 @@ if (typeof window.ethereum !== "undefined") {
 
 // Function to connect to Polygon network
 async function connectToPolygon() {
-  // Request access to the user's MetaMask accounts
-  await window.ethereum.request({ method: "eth_requestAccounts" });
-
-  // Get the current network ID
-  const chainId = await window.ethereum.request({ method: "eth_chainId" });
-
-  // Check if the current network is Polygon (Matic)
-  if (chainId === "0x89" || chainId === "137") {
-    console.log("Connected to Polygon network!");
-    // You can perform further actions on the Polygon network here
-  } else {
-    // Switch to Polygon network in MetaMask
-    try {
-      await window.ethereum.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: "0x89" }], // Polygon Mainnet
-      });
-      console.log("Switched to Polygon network!");
-      // You can perform further actions on the Polygon network here
-    } catch (error) {
-      console.log("Failed to switch to Polygon network:", error);
-    }
-  }
+  await connectToNetwork("Polygon", "0x89");
 }
 
+// Function to connect to Meter network
 async function connectToMeter() {
-  // Request access to the user's MetaMask accounts
-  await window.ethereum.request({ method: "eth_requestAccounts" });
-
-  // Get the current network ID
-  const chainId = await window.ethereum.request({ method: "eth_chainId" });
-
-  // Check if the current network is Polygon (Matic)
-  if (chainId === "0x52" || chainId === "82") {
-    console.log("Connected to Polygon network!");
-    // You can perform further actions on the Polygon network here
-  } else {
-    // Switch to Polygon network in MetaMask
-    try {
-      await window.ethereum.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: "0x52" }], // Polygon Mainnet
-      });
-      console.log("Switched to Polygon network!");
-      // You can perform further actions on the Polygon network here
-    } catch (error) {
-      console.log("Failed to switch to Polygon network:", error);
-    }
-  }
+  await connectToNetwork("Meter", "0x52");
 }
 
+// Function to connect to Telos network
 async function connectToTelos() {
-  // Request access to the user's MetaMask accounts
-  await window.ethereum.request({ method: "eth_requestAccounts" });
+  await connectToNetwork("Telos", "0x28");
+}
 
-  // Get the current network ID
-  const chainId = await window.ethereum.request({ method: "eth_chainId" });
+// Function to connect to a specific network
+async function connectToNetwork(networkName, networkId) {
+  try {
+    // Request access to the user's MetaMask accounts
+    await window.ethereum.request({ method: "eth_requestAccounts" });
 
-  // Check if the current network is Polygon (Matic)
-  if (chainId === "0x28" || chainId === "40") {
-    console.log("Connected to Polygon network!");
-    // You can perform further actions on the Polygon network here
-  } else {
-    // Switch to Polygon network in MetaMask
-    try {
-      await window.ethereum.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: "0x28" }], // Polygon Mainnet
-      });
-      console.log("Switched to Polygon network!");
-      // You can perform further actions on the Polygon network here
-    } catch (error) {
-      console.log("Failed to switch to Polygon network:", error);
+    // Get the current network ID
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+
+    // Check if the current network matches the target network
+    if (chainId === networkId) {
+      console.log(`Connected to ${networkName} network!`);
+      // You can perform further actions on the target network here
+    } else {
+      // Switch to the target network in MetaMask
+      try {
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: networkId }],
+        });
+        console.log(`Switched to ${networkName} network!`);
+        // You can perform further actions on the target network here
+      } catch (error) {
+        console.log(`Failed to switch to ${networkName} network:`, error);
+      }
     }
+  } catch (error) {
+    console.log(`Failed to connect to ${networkName} network:`, error);
   }
 }
