695aca16fc64881cc1c7da12e62e2d5e6589e465
XLazer4 <khannarahul446@gmail.com>
2023-06-09 17:12:31 +0530

    logic for meter and telos

--- app.js
+++ app.js
@@ -29,3 +29,55 @@ async function connectToPolygon() {
     }
   }
 }
+
+async function connectToMeter() {
+  // Request access to the user's MetaMask accounts
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+
+  // Get the current network ID
+  const chainId = await window.ethereum.request({ method: "eth_chainId" });
+
+  // Check if the current network is Polygon (Matic)
+  if (chainId === "0x52" || chainId === "82") {
+    console.log("Connected to Polygon network!");
+    // You can perform further actions on the Polygon network here
+  } else {
+    // Switch to Polygon network in MetaMask
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x52" }], // Polygon Mainnet
+      });
+      console.log("Switched to Polygon network!");
+      // You can perform further actions on the Polygon network here
+    } catch (error) {
+      console.log("Failed to switch to Polygon network:", error);
+    }
+  }
+}
+
+async function connectToTelos() {
+  // Request access to the user's MetaMask accounts
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+
+  // Get the current network ID
+  const chainId = await window.ethereum.request({ method: "eth_chainId" });
+
+  // Check if the current network is Polygon (Matic)
+  if (chainId === "0x28" || chainId === "40") {
+    console.log("Connected to Polygon network!");
+    // You can perform further actions on the Polygon network here
+  } else {
+    // Switch to Polygon network in MetaMask
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x28" }], // Polygon Mainnet
+      });
+      console.log("Switched to Polygon network!");
+      // You can perform further actions on the Polygon network here
+    } catch (error) {
+      console.log("Failed to switch to Polygon network:", error);
+    }
+  }
+}
